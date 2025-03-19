c03c63aa45e8a9813d46107b8d393b42980419a9
SaraZez0 <sara.zezo252@gmail.com>
2025-03-19 10:11:27 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from "./components/SideBar"
 
 import SignIn from './pages/sign_in/SignIn';
